619d880c3670fbea53468680447c347e18aef8d0
areebatahir11 <areebatahir338@gmail.com>
2026-03-11 22:39:27 +0500

    enum  for Status VerificationTYpes  complaintStructs and events are added

--- blockchain-layer/contracts/streetLight.sol
+++ blockchain-layer/contracts/streetLight.sol
@@ -1,9 +1,25 @@
-//SPDX-License-Identifier: MIT
-
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-contract StreetLight{
+contract StreetLight {
+    enum Status {
+        VERIFIED,
+        RESOLVED
+    }
+    enum VerificationType {
+        AUTO,
+        MANUAL
+    }
+    struct complaint {
+        uint256 complaintId;
+        bytes32 imageHash;
+        string issueType;
+        bytes32 locationHash;
+        uint256 timestamp;
+        VerificationType verificationType;
+        Status status;
+    }
 
-    
+    event ComplainVerification(uint256 complaintID, bytes32 imagehash, string issueType, uint256 timestamp, Status s);
+    event ComplainResolved(uint256 complaintID, bytes32 imagehash, string issueType, uint256 timestamp, Status s);
 }
-
