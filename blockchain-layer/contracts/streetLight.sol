4c489b688ed573f6038c52d8b19575edb91705b3
areebatahir11 <areebatahir338@gmail.com>
2026-03-12 00:04:53 +0500

    Add Authorization

--- blockchain-layer/contracts/streetLight.sol
+++ blockchain-layer/contracts/streetLight.sol
@@ -19,7 +19,8 @@ contract StreetLight is Ownable {
     struct Complaint {
         uint256 complaintId;
         bytes32 imageHash;
-        string issueType;
+        uint8 issueType;
+        uint256 timestamp;
         bytes32 locationHash;
         VerificationType verificationType;
         Status status;
@@ -28,35 +29,41 @@ contract StreetLight is Ownable {
     mapping(uint256 => Complaint) public complaints;
 
     event ComplainVerified(
-        uint256 complaintID,
+        uint256 indexed complaintID,
         bytes32 imagehash,
-        string issueType,
+        uint8 issueType,
         uint256 timestamp,
         Status s
     );
     event ComplainResolved(
-        uint256 complaintID,
+        uint256 indexed complaintID,
         bytes32 imagehash,
-        string issueType,
-        uint256 timestamp,
-        Status s
+        uint8 issueType,
+        uint256 timestamp
     );
 
     function registerComplaint(
         uint256 complaintID,
         bytes32 imagehash,
-        string memory issueType,
+        uint8 issueType, //Will use category-num like 1-pothole 2-garbage
         bytes32 locationHash,
         VerificationType v
     ) public onlyOwner {
-        complaints[complaintID] = Complaint(
-            complaintID,
-            imagehash,
-            issueType,
-            locationHash,
-            v,
-            Status.VERIFIED
+        require(
+            complaints[complaintID].complaintId == 0,
+            "Complaint already exists"
         );
+        complaints[complaintID] = Complaint({
+            complaintId: complaintID,
+            imageHash: imagehash,
+            locationHash: locationHash,
+            issueType: issueType,
+            timestamp: block.timestamp,
+            verificationType: v,
+            status: Status.VERIFIED
+        });
+        TotalComplaints++;
+
         emit ComplainVerified(
             complaintID,
             imagehash,
@@ -64,41 +71,44 @@ contract StreetLight is Ownable {
             block.timestamp,
             Status.VERIFIED
         );
-        TotalComplaints++;
     }
 
     function GetComplaint(
         uint256 ComplaintID
     ) public view returns (Complaint memory c) {
+        require(
+            complaints[ComplaintID].complaintId != 0,
+            "Complaint does not exist"
+        );
         return complaints[ComplaintID];
     }
 
-    function ComplaintResolved(
-        uint256 complaintID,
-        bytes32 imagehash,
-        string memory issueType,
-        bytes32 locationHash,
-        VerificationType v
-    ) public onlyOwner {
-        complaints[complaintID] = Complaint(
-            complaintID,
-            imagehash,
-            issueType,
-            locationHash,
-            v,
-            Status.RESOLVED
+    function markResolved(uint256 complaintID) public onlyOwner {
+        require(
+            complaints[complaintID].complaintId != 0,
+            "Complaint does not exist"
         );
+        require(
+            complaints[complaintID].status != Status.RESOLVED,
+            "Already resolved"
+        );
+        complaints[complaintID].status = Status.RESOLVED;
+
         emit ComplainResolved(
             complaintID,
-            imagehash,
-            issueType,
-            block.timestamp,
-            Status.RESOLVED
+            complaints[complaintID].imageHash,
+            complaints[complaintID].issueType,
+            block.timestamp
         );
     }
+
     function GetComplaintHash(
         uint256 complainID
     ) public view returns (bytes32 complainHash) {
+        require(
+            complaints[complainID].complaintId != 0,
+            "Complaint does not exist"
+        );
         return complaints[complainID].imageHash;
     }
     function getTotalComplaints() public view returns (uint256) {
