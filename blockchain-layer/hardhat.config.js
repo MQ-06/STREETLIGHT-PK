a98af70973edef21a91051b5642a6a3abc954e09
areebatahir11 <areebatahir338@gmail.com>
2026-05-10 11:21:01 +0500

    fix: configure hardhat for Sepolia testnet deployment

--- blockchain-layer/hardhat.config.js
+++ blockchain-layer/hardhat.config.js
@@ -1,6 +1,23 @@
-require("@nomicfoundation/hardhat-toolbox");
+// require("@nomicfoundation/hardhat-toolbox");
 
-/** @type import('hardhat/config').HardhatUserConfig */
+// /** @type import('hardhat/config').HardhatUserConfig */
+// module.exports = {
+//   solidity: "0.8.28",
+// };
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
 module.exports = {
-  solidity: "0.8.28",
+  solidity: "0.8.20",
+
+  networks: {
+    sepolia: {
+      url: SEPOLIA_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 11155111,
+    },
+  },
 };
